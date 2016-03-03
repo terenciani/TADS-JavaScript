90b4508026e51d231c2020f8835312b2a765af0b
Marcelo F. Terenciani <marcelo.figterenciani@gmail.com>
2016-03-03 17:47:01 -0400

    Aula 1 - Correcao javaScript

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-var teste = 1;
+troca = 1;
 
 function mudarCores() {
-	if (teste == 1){
+	if (troca == 1){
 		document.getElementById("cabecalho").style.background = "black";
 		document.getElementById("conteudo").style.background = "red";
 		document.getElementById("rodape").style.background = "black";
-		teste = 0;
+		troca = 0;
 	} else{
 		document.getElementById("cabecalho").style.background = "red";
 		document.getElementById("conteudo").style.background = "black";
 		document.getElementById("rodape").style.background = "red";
-		teste = 1;
+		troca = 1;
 	}
 }
